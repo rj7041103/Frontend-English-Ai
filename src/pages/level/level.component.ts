7bd2ea8336eb75ce8377fd0b1c1aabe7d3d0526b
rj7041103 <jeyjeyelcracv@gmail.com>
2025-03-21 23:31:15 -0400

    add level and rearrange component in page folder

---
+++ src/pages/level/level.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-level',
+  imports: [],
+  templateUrl: './level.component.html',
+  styleUrl: './level.component.css'
+})
+export class LevelComponent {
+
+}
